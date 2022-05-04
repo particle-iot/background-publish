e83f86f10bed82465eeda36294b271275944480b
Ed Ablan <1463248+edablan@users.noreply.github.com>
2022-05-04 08:51:17 -0700

    Added a cb wrapper to allow callbacks functions from a class not just a static function

--- test/test.cpp
+++ test/test.cpp
@@ -32,7 +32,9 @@ void priority_low_cb(publishStatus status,
 TEST_CASE("Test Background Publish") {
     BackgroundPublish::instance().init();
 
-    //FAIL, not enough entries
+    BackgroundPublish::instance().thread_f(); //run once at the top to establish
+    //a start time for the thread_f
+
     for(int i = 0; i < NUM_ENTRIES; i++) {
     BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
                         str.c_str(), 
@@ -69,51 +71,48 @@ TEST_CASE("Test Background Publish") {
                         PRIVATE,
                         1, 
                         priority_high_cb ) == true);
-    BackgroundPublish::instance().thread_f(); //run to clear off of the queues
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f(); //run to clear off the queues
+
+    //Fail, not enough time passed between processing publishes
+    //Then PASS once once time has passed.
+    high_cb_counter = 0;
+    low_cb_counter = 0;
+    status_returned = publishStatus::PUBLISH_COMPLETE;
+    Particle.state_output.isDoneReturn = true;
+    Particle.state_output.isSucceededReturn = true;
+    REQUIRE(BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
+                        str.c_str(), 
+                        PRIVATE,
+                        0, 
+                        priority_high_cb ) == true);
+    System.inc(500); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f(); //run to clear off the queues
+    REQUIRE(high_cb_counter == 0);
+    REQUIRE(low_cb_counter == 0);
+    System.inc(500); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f(); //run to clear off the queues
+    REQUIRE(high_cb_counter == 1);
+    REQUIRE(low_cb_counter == 0);
 
     //PUBLISH_BUSY, run thread_f and fail on is.Succeeded()
     high_cb_counter = 0;
+    low_cb_counter = 0;
     status_returned = publishStatus::PUBLISH_COMPLETE;
     Particle.state_output.isDoneReturn = true;
     Particle.state_output.isSucceededReturn = false;
     BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
                         str.c_str(), 
                         PRIVATE,
-                        1, 
+                        0, 
                         priority_high_cb);
-
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
     BackgroundPublish::instance().thread_f();
     REQUIRE(high_cb_counter == 1);
+    REQUIRE(low_cb_counter == 0);
     REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
 
     //PUBLISH_COMPLETE, run thread_f and pass on is.Succeeded()
-    high_cb_counter = 0;
-    status_returned = publishStatus::PUBLISH_BUSY;
-    Particle.state_output.isDoneReturn = true;
-    Particle.state_output.isSucceededReturn = true;
-    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
-                        str.c_str(), 
-                        PRIVATE,
-                        1, 
-                        priority_high_cb);
-    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
-                        str.c_str(), 
-                        PRIVATE,
-                        1, 
-                        priority_high_cb);
-    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
-                        str.c_str(), 
-                        PRIVATE,
-                        1, 
-                        priority_high_cb);
-
-    BackgroundPublish::instance().thread_f();
-    BackgroundPublish::instance().thread_f();
-    BackgroundPublish::instance().thread_f();
-    REQUIRE(high_cb_counter == 3);
-    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
-
-    //PUBLISH_COMPLETE, publish from high and low priority queues
     high_cb_counter = 0;
     low_cb_counter = 0;
     status_returned = publishStatus::PUBLISH_BUSY;
@@ -122,48 +121,116 @@ TEST_CASE("Test Background Publish") {
     BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
                         str.c_str(), 
                         PRIVATE,
-                        1, 
+                        0, 
                         priority_high_cb);
     BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
                         str.c_str(), 
                         PRIVATE,
-                        1, 
+                        0, 
                         priority_high_cb);
     BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
                         str.c_str(), 
                         PRIVATE,
-                        1, 
+                        0, 
                         priority_high_cb);
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
+    status_returned = publishStatus::PUBLISH_BUSY; //clearout to something
 
-    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
-                        str.c_str(), 
-                        PRIVATE,
-                        0, 
-                        priority_low_cb);
-    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
-                        str.c_str(), 
-                        PRIVATE,
-                        0, 
-                        priority_low_cb);
-    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
-                        str.c_str(), 
-                        PRIVATE,
-                        0, 
-                        priority_low_cb);
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
+    status_returned = publishStatus::PUBLISH_BUSY; //clearout to something
 
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
     BackgroundPublish::instance().thread_f();
     REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
-    BackgroundPublish::instance().thread_f();
-    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
-    BackgroundPublish::instance().thread_f();
-    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
-    Particle.state_output.isSucceededReturn = false;
-    BackgroundPublish::instance().thread_f();
-    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
-    BackgroundPublish::instance().thread_f();
-    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
-    BackgroundPublish::instance().thread_f();
-    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
+    status_returned = publishStatus::PUBLISH_BUSY; //clearout to something
     REQUIRE(high_cb_counter == 3);
+    REQUIRE(low_cb_counter == 0);
+
+    //PUBLISH_COMPLETE, publish from high and low priority queues
+    high_cb_counter = 0;
+    low_cb_counter = 0;
+    status_returned = publishStatus::PUBLISH_CLEANUP;
+    Particle.state_output.isDoneReturn = true;
+    Particle.state_output.isSucceededReturn = true;
+    BackgroundPublish::instance().publish("TEST_PUB_LOW", 
+                        str.c_str(), 
+                        PRIVATE,
+                        1, 
+                        priority_low_cb);
+    BackgroundPublish::instance().publish("TEST_PUB_LOW", 
+                        str.c_str(), 
+                        PRIVATE,
+                        1, 
+                        priority_low_cb);
+    BackgroundPublish::instance().publish("TEST_PUB_LOW", 
+                        str.c_str(), 
+                        PRIVATE,
+                        1, 
+                        priority_low_cb);
+
+    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
+                        str.c_str(), 
+                        PRIVATE,
+                        0, 
+                        priority_high_cb);
+    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
+                        str.c_str(), 
+                        PRIVATE,
+                        0, 
+                        priority_high_cb);
+    BackgroundPublish::instance().publish("TEST_PUB_HIGH", 
+                        str.c_str(), 
+                        PRIVATE,
+                        0, 
+                        priority_high_cb);
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
+    REQUIRE(low_cb_counter == 0);
+    REQUIRE(high_cb_counter == 1);
+    status_returned = publishStatus::PUBLISH_CLEANUP; //clear out to something
+
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
+    REQUIRE(low_cb_counter == 0);
+    REQUIRE(high_cb_counter == 2);
+    status_returned = publishStatus::PUBLISH_CLEANUP; //clear out to something
+
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_COMPLETE);
+    REQUIRE(low_cb_counter == 0);
+    REQUIRE(high_cb_counter == 3);
+    status_returned = publishStatus::PUBLISH_CLEANUP; //clear out to something
+
+    Particle.state_output.isSucceededReturn = false;
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
+    REQUIRE(low_cb_counter == 1);
+    REQUIRE(high_cb_counter == 3);
+    status_returned = publishStatus::PUBLISH_CLEANUP; //clear out to something
+
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
+    REQUIRE(low_cb_counter == 2);
+    REQUIRE(high_cb_counter == 3);
+    status_returned = publishStatus::PUBLISH_CLEANUP; //clear out to something
+
+    System.inc(1000); //increase the tick by one second to allow thread_f to process
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
+    BackgroundPublish::instance().thread_f();
+    REQUIRE(status_returned == publishStatus::PUBLISH_BUSY);
     REQUIRE(low_cb_counter == 3);
+    REQUIRE(high_cb_counter == 3);
+    status_returned = publishStatus::PUBLISH_CLEANUP; //clear out to something
 }
